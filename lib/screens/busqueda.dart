56283340a5d8e24a529375f24dacd4593e2c1023
matypinkman <matias1998@hotmail.com>
2022-12-16 05:20:43 -0300

    Busqueda de pokemon funcionando

--- lib/screens/busqueda.dart
+++ lib/screens/busqueda.dart
@@ -3,7 +3,7 @@
 import 'dart:convert';
 
 import 'package:flutter/material.dart';
-import 'package:flutter/services.dart';
+
 import 'package:http/http.dart' as http;
 
 class Busqueda extends StatefulWidget {
@@ -14,43 +14,95 @@ class Busqueda extends StatefulWidget {
 }
 
 class _BusquedaState extends State<Busqueda> {
-  TextEditingController _idvalue = TextEditingController();
+  int _pokemonId = 1;
+
+  Map _pokemonInfo = {};
+  final _pokemonIdController = TextEditingController();
+  Future<void> _fetchPokemonInfo() async {
+    // Obtiene el ID del Pokémon del controlador de texto
+    _pokemonId = int.parse(_pokemonIdController.text);
+    // Realiza una petición HTTP para obtener la información del Pokémon
+
+    final response = await http
+        .get(Uri.parse('https://pokeapi.co/api/v2/pokemon/${_pokemonId}'));
+
+    // Si la petición fue exitosa, decodifica la respuesta y guarda la información del Pokémon
+    if (response.statusCode == 200) {
+      setState(() {
+        _pokemonInfo = json.decode(response.body);
+      });
+    }
+  }
+
+  late Map<String, dynamic> _pokemon;
+
   @override
   Widget build(BuildContext context) {
-    return Column(
-      mainAxisAlignment: MainAxisAlignment.start,
-      children: <Widget>[
-        TextField(
-          controller: _idvalue,
-          decoration: InputDecoration(
-            border: const OutlineInputBorder(),
-            hintText: "Ingrese el ID del Pokemon",
-          ),
-          inputFormatters: [FilteringTextInputFormatter.digitsOnly],
-        ),
-        ElevatedButton(
-            style: ElevatedButton.styleFrom(
-              backgroundColor: Colors.black87,
+    return Scaffold(
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Column(
+          children: [
+            TextField(
+              controller: _pokemonIdController,
+              decoration: InputDecoration(labelText: 'ID del Pokemón'),
             ),
-            onPressed: () {
-              String ID_Poke = _idvalue.text;
-              var url = "pokeapi.co";
-              var urlid = "/api/v2/pokemon/$ID_Poke";
-              fetchPokeData(url, urlid);
-            },
-            child: Text("Buscar"))
-      ],
+            ElevatedButton(
+                style: ElevatedButton.styleFrom(
+                  backgroundColor: Colors.black87,
+                ),
+                onPressed: _fetchPokemonInfo,
+                child: Text("Buscar")),
+            SizedBox(
+              width: 450,
+              height: 350,
+              child: Card(
+                elevation: 10,
+                shape: RoundedRectangleBorder(
+                    side: BorderSide(
+                      color: Colors.black,
+                    ),
+                    borderRadius: BorderRadius.circular(10.0)),
+                color: Colors.white,
+                child: _pokemonInfo != null
+                    ? Column(
+                        mainAxisSize: MainAxisSize.max,
+                        children: [
+                          Image.network(
+                            _pokemonInfo['sprites']?['front_default'] ??
+                                'http://geniussys.com/img/placeholder/blogpost-placeholder-100x100.png',
+                            width: 100,
+                            height: 100,
+                          ),
+                          Text(
+                              style: TextStyle(fontSize: 25),
+                              'ID: ${_pokemonInfo['id']}'),
+                          Text(
+                              style: TextStyle(fontSize: 25),
+                              'Nombre: ${_pokemonInfo['name']}'),
+                          Text(
+                              style: TextStyle(fontSize: 25),
+                              'Altura: ${_pokemonInfo['height']}'),
+                          Text(
+                              style: TextStyle(fontSize: 25),
+                              'Peso: ${_pokemonInfo['weight']}'),
+                          Text(
+                              style: TextStyle(fontSize: 25),
+                              'Tipos: ${_pokemonInfo['types']?.map((type) => type['type']['name']).join(', ') ?? 'Sin tipos'}'),
+                          Text(
+                              style: TextStyle(fontSize: 25),
+                              'Formas: ${_pokemonInfo['forms']?.map((form) => form['name']).join(', ') ?? 'Sin formas'}'),
+                          Text(
+                              style: TextStyle(fontSize: 25),
+                              'Habilidades: ${_pokemonInfo['abilities']?.map((ability) => ability['ability']['name']).join(', ') ?? 'Sin habilidades'}'),
+                        ],
+                      )
+                    : Container(),
+              ),
+            )
+          ],
+        ),
+      ),
     );
   }
-}
-
-void fetchPokeData(link, id) {
-  List Pokedex;
-  var url = Uri.http(link, id);
-  http.get(url).then((value) {
-    if (value.statusCode == 200) {
-      var dataJson = jsonDecode(value.body);
-      print(dataJson['name']);
-    }
-  });
 }
